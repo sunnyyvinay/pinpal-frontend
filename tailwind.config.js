274c8b3b6c73a4c10c7d30f8545a30cd5670bc6b
Sunny Vinay <sunnyvinay7@gmail.com>
2024-08-13 14:09:26 -0700

    remove tailwind

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/screens/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
